b443ecbffa2989bbad649fee7c0186d090f6be24
dptytlapampa-code <dptyt.lapampa@gmail.com>
2026-02-21 18:05:37 -0300

    Seed EquipoStatusSeeder in base test setup

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -2,8 +2,18 @@
 
 namespace Tests;
 
+use Database\Seeders\EquipoStatusSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(EquipoStatusSeeder::class);
+    }
 }
