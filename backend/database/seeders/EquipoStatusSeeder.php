73c05d3b00ccfaec701c8905caf4250c35554dce
dptytlapampa-code <dptyt.lapampa@gmail.com>
2026-03-12 12:26:01 -0300

    Audita flujo de préstamos

--- backend/database/seeders/EquipoStatusSeeder.php
+++ backend/database/seeders/EquipoStatusSeeder.php
@@ -9,15 +9,7 @@ class EquipoStatusSeeder extends Seeder
 {
     public function run(): void
     {
-        $statuses = [
-            ['code' => EquipoStatus::CODE_OPERATIVA, 'name' => 'Operativa', 'color' => 'green', 'is_terminal' => false],
-            ['code' => EquipoStatus::CODE_PRESTADA, 'name' => 'Prestada', 'color' => 'blue', 'is_terminal' => false],
-            ['code' => EquipoStatus::CODE_EN_SERVICIO_TECNICO, 'name' => 'En Servicio Tecnico', 'color' => 'yellow', 'is_terminal' => false],
-            ['code' => EquipoStatus::CODE_FUERA_DE_SERVICIO, 'name' => 'Fuera de Servicio', 'color' => 'orange', 'is_terminal' => false],
-            ['code' => EquipoStatus::CODE_BAJA, 'name' => 'Baja', 'color' => 'red', 'is_terminal' => true],
-        ];
-
-        foreach ($statuses as $status) {
+        foreach (EquipoStatus::canonicalDefinitions() as $status) {
             EquipoStatus::query()->updateOrCreate(['code' => $status['code']], $status);
         }
     }
